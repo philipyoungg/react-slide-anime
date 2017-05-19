5dd32f2d5ac4cfbfe4dfdfe279994b51584d41b3
Philip Young <philip.young.gunawan@gmail.com>
2017-05-20 00:09:59 +0700

    moved variables to component

--- src/App.js
+++ src/App.js
@@ -9,23 +9,24 @@ const Slide = styled.div`
   height: 100vh;
   top: 0;
   opacity: ${p => (p.offset ? p.offset / 10 : 0)}
-  left: ${p => (p.offset ? p.offset * 100 : 0)}%
-  background: red;
+  left: ${p => (p.offset ? (p.offset - 1) * 100 : 0)}%
+  background: ${p => (p.color ? p.color : "red")};
 `;
 
 class App extends Component {
   render() {
+    const { totalSlide, duration } = this.props;
     return (
       <div className="w-100 vh-100 relative overflow-hidden">
         <Anime
-          duration={3000}
+          duration={duration}
           easing="easeInOutQuad"
           direction="alternate"
           loop={true}
-          translateX={[0, "-1000vw"]}
+          translateX={[0, `-${(totalSlide - 1) * 100}vw`]}
         >
           <div>
-            {range(1, 11).map(i => <Slide offset={i} />)}
+            {range(1, totalSlide + 1).map(i => <Slide offset={i} />)}
           </div>
         </Anime>
       </div>
